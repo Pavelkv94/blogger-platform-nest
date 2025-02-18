47ec2ee5f69b9e35f5d2d485cb30dbac1ba774ba
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2025-02-18 11:31:41 +0300

    add env to migration source

--- migrations/data-source.ts
+++ migrations/data-source.ts
@@ -1,12 +1,26 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config({
+  path: [
+    // high priority
+    ...(process.env.ENV_FILE_PATH ? [process.env.ENV_FILE_PATH.trim()] : []),
+    // lower priority
+    `.env.${process.env.NODE_ENV}.local`,
+    // lower priority
+    `.env.${process.env.NODE_ENV}`,
+    // lower priority
+    '.env.production',
+  ]
+});
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost', // Адрес вашего сервера PostgreSQL
+  host: process.env.DB_HOST, // Адрес вашего сервера PostgreSQL
   port: 5432, // Порт по умолчанию
-  username: 'admin', // Ваше имя пользователя
-  password: 'admin', // Ваш пароль
-  database: 'migro2', // Имя новой базы данных
+  username: process.env.DB_USERNAME, // Ваше имя пользователя
+  password: process.env.DB_PASSWORD, // Ваш пароль
+  database: process.env.DB_NAME, // Имя новой базы данных
   migrations: [__dirname + '/data/**/*{.ts,.js}'],
   entities: ['src/**/*.entity{.ts,.js}'], // Здесь укажите ваши сущности
 });
